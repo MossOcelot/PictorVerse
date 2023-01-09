110367f5b784b78e0d4d405cf4e2e7c859d5299b
sittaInwza007 <sittasahathum@gmail.com>
2023-01-09 13:30:16 +0700

    add new Section folder

---
+++ Assets/Scripts/Camera/OtherCameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class OtherCameraController : MonoBehaviour
+{
+    Transform target;
+    Vector3 velocity = Vector3.zero;
+
+
+    //กำหนดความยาวช่วงของ smooth camera
+    [Range(0,1)]
+    public float smoothTime ;
+
+    public Vector3 positionOffset;
+
+    private void Awake()
+    {
+
+        //ทำงานใน Tag Player
+        target = GameObject.FindGameObjectWithTag("Player").transform; 
+    }
+
+    private void LateUpdate()
+    {
+
+        //จัดการมุมกล้อง
+        Vector3 targetposition = target.position+positionOffset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetposition, ref velocity, smoothTime);
+    }
+}
