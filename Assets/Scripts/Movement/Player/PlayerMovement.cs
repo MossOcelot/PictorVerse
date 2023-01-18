43fc7088e6d906810c1e87c52b8d25c6c28bdd9d
MossOcelot <108613206+MossOcelot@users.noreply.github.com>
2023-01-18 18:42:26 +0700

    add PlayerStatus

--- Assets/Scripts/Movement/Player/PlayerMovement.cs
+++ Assets/Scripts/Movement/Player/PlayerMovement.cs
@@ -7,28 +7,57 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
+    [SerializeField]
+    private PlayerStatus status;
     private Rigidbody2D rb;
     public Animator animator;
 
+    public float walk_distance = 0;
+    private bool iswalk;
+
+    [SerializeField]
+    private int energy_for_walk;
+    Vector2 playerposition;
 
     Vector2 movement;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerposition = transform.position;
     }
     private void Update()
     {
         Movement();
+        if (movement.x != 0 || movement.y != 0) { 
+            iswalk = true;
+        } else
+        {
+            iswalk = false;
+        }
+        count_distance_for_walk();
+
+
     }
 
     
     private void Movement()
     {
+        int energy = status.getEnergy();
+        if (energy == 0)
+        {
+            moveSpeed = 1f;
+        } else
+        {
+            moveSpeed = 5f;
+        }
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         AnimationMovement();
+        
+        
     }
     private void AnimationMovement()
     {
@@ -42,6 +71,26 @@ public class PlayerMovement : MonoBehaviour
         if (target.gameObject.CompareTag("Monster"))
         {
             Destroy(target.gameObject);
-        }        
+        }
     }
+
+    private void count_distance_for_walk() {
+        if(iswalk)
+        {
+           var distance = Vector2.Distance(transform.position, playerposition);
+           if (distance >= 10)
+            {
+                walk_distance += 10;
+                playerposition = transform.position;
+                useEnergy(energy_for_walk);
+            }
+        }
+    }
+
+    private void useEnergy(int energy)
+    {
+        status.setEnergy(-energy);
+        status.static_useEnergy += energy;
+    }
+
 }
