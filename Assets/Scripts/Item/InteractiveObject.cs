1afd1a726273a812ccf15111a7ca75649b16ae2e
Pathitta Suksomboon <praewiizzpraew@gmail.com>
2023-01-22 02:53:51 +0700

    all current Inventory Scripts

---
+++ Assets/Scripts/Item/InteractiveObject.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float radius = 3f; //initial distance of interact
+    public Transform player;
+    public Transform interactItem;
+    bool hasInteract = false; // check if touching an item
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(player.position, interactItem.position);
+        if (distance <= radius && !hasInteract)
+        {
+            hasInteract = true;
+            Interact();
+        }
+    }
+
+    public virtual void Interact()
+    {
+
+    }
+    //create pick up area
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactItem.position, radius);
+    }
+}
