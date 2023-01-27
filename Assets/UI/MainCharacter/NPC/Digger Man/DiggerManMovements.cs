756f67947926284f812b636689e58eb6fe152770
sittaInwza007 <sittasahathum@gmail.com>
2023-01-27 14:11:25 +0700

    Update friday work

--- Assets/UI/MainCharacter/NPC/Digger Man/DiggerManMovements.cs
+++ Assets/UI/MainCharacter/NPC/Digger Man/DiggerManMovements.cs
@@ -2,81 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterMovements : MonoBehaviour
+public class DiggerManMovements : MonoBehaviour
 {
     private Vector3 previousVelocity;
 
     private bool isMoving;
 
-    private Vector3 directionVector;
-    private Transform myTransform;
     public float speed;
     private Rigidbody2D myRigidbody;
     private Animator anim;
-    public Collider2D bounds;
-    
-    
+
     private void Start()
     {
         anim = GetComponent<Animator>();
-        myTransform = GetComponent<Transform>();
         myRigidbody = GetComponent<Rigidbody2D>();
-        ChangeDirection();
+
     }
 
     private void Update()
     {
-        Move();
+        
         anim.SetBool("isMoving", isMoving);
 
-    }
-
-    private void Move()
-    {
-        Vector3 temp = myTransform.position + directionVector * speed * Time.deltaTime;
-        if (bounds.bounds.Contains(temp))
-        {
-            myRigidbody.MovePosition(temp);
-        }
-        else
-        {
-            ChangeDirection();
-        }
-    }
-    void ChangeDirection()
-    {
-        int direction = Random.Range(0, 4);
-
-        switch (direction)
-        {
-            case 0:
-                //ขวา
-                directionVector = Vector3.right;
-                break;
-            case 1:
-                directionVector = Vector3.up;
-                //บน
-                break;
-            case 2:
-                directionVector = Vector3.left;
-                //ซ้าย
-                break;
-            case 3:
-                directionVector = Vector3.down;
-                //ล่าง
-                break;
-            default:
-                break;
-
-        }
-        UpdateAnimation();
-    }
-
-    void UpdateAnimation()
-    {
-        anim.SetFloat("MoveX", directionVector.x);
-        anim.SetFloat("MoveY", directionVector.y);
-        anim.SetFloat("Speed", directionVector.sqrMagnitude);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
