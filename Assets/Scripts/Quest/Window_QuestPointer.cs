63c001268d1f92da6358aaba3dd351ae1f2361e1
Sitta Sahathum <sittasahathum@gmail.com>
2023-02-27 04:42:06 +0700

    add some compass

---
+++ Assets/Scripts/Quest/Window_QuestPointer.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+
+public class Window_QuestPointer : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private Image pointerImage;
+    private RectTransform pointerRectTransform;
+    [SerializeField] private Sprite arrowSprite;
+    [SerializeField] private Sprite crossSprite;
+    private Camera uiCamera;
+    [SerializeField] private Vector2 targetPositionInInspector = new Vector2(200, 45);
+
+
+    private void Awake()
+    {
+        targetPosition = targetPositionInInspector;
+        pointerRectTransform = transform.Find("Pointer").GetComponent<RectTransform>();
+        pointerImage = transform.Find("Pointer").GetComponent<Image>();
+
+    }
+
+    private void Update()
+    {
+        Vector3 toPosition = targetPosition;
+        Vector3 fromPosition = Camera.main.transform.position;
+
+        fromPosition.z = 0f;
+        Vector3 dir = (toPosition - fromPosition).normalized;
+
+        float angle = UtilsClass.GetAngleFromVectorFloat(dir);
+        pointerRectTransform.localEulerAngles = new Vector3(0, 0, angle);
+
+
+        float borderSize = 100f; 
+        Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(targetPosition);
+        bool isOffScreen = targetPositionScreenPoint.x <= borderSize || targetPositionScreenPoint.x >= Screen.width - borderSize || targetPositionScreenPoint.y <= borderSize || targetPositionScreenPoint.y >= Screen.height - borderSize ;
+        if (isOffScreen)
+        {
+            RotatePointerTowardsTargerPosition();
+            pointerImage.sprite = arrowSprite;
+            Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
+            if (cappedTargetScreenPosition.x <= 0) cappedTargetScreenPosition.x = borderSize;
+            if (cappedTargetScreenPosition.x >= Screen.width - borderSize) cappedTargetScreenPosition.x = Screen.height;
+            if (cappedTargetScreenPosition.y <= 0) cappedTargetScreenPosition.y = borderSize;
+            if (cappedTargetScreenPosition.y >= Screen.height - borderSize) cappedTargetScreenPosition.y = Screen.height;
+
+            Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(cappedTargetScreenPosition);
+            pointerRectTransform.position = pointerWorldPosition;
+            pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f);
+
+        }
+        else
+        {
+            pointerImage.sprite = crossSprite;
+
+            Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(targetPositionScreenPoint);
+
+            pointerRectTransform.position = pointerWorldPosition;
+            pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f);
+            pointerRectTransform.localEulerAngles = Vector3.zero;
+
+        }
+    }
+
+    private void RotatePointerTowardsTargerPosition()
+    {
+        Vector3 toPosition = targetPosition;
+        Vector3 fromPosition = Camera.main.transform.position;
+        fromPosition.z = 0f;
+        Vector3 dir = (toPosition - fromPosition).normalized;
+        float angle = UtilsClass.GetAngleFromVectorFloat(dir);
+        pointerRectTransform.localEulerAngles = new Vector3(0, 0, angle);
+    }
+
+}
