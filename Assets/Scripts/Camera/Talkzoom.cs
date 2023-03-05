98c3dd8118b086b61ea14e45d100f5d5d417700b
Sitta Sahathum <sittasahathum@gmail.com>
2023-03-05 18:34:08 +0700

    add camera zoom while Talk Banking and add NPC Bank

---
+++ Assets/Scripts/Camera/Talkzoom.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Talkzoom : MonoBehaviour
+{
+    public Camera cameraToZoom; // assign camera to zoom in the Unity Editor
+    public float zoomFactor = 1.2f;
+    public float smoothTime = 0.5f;
+
+    private float targetSize;
+    private float currentVelocity;
+
+    void Start()
+    {
+        targetSize = cameraToZoom.orthographicSize;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            targetSize /= zoomFactor;
+            StartCoroutine(SmoothZoom());
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            targetSize *= zoomFactor;
+            StartCoroutine(SmoothZoom());
+        }
+    }
+
+    IEnumerator SmoothZoom()
+    {
+        while (Mathf.Abs(cameraToZoom.orthographicSize - targetSize) > 0.01f)
+        {
+            cameraToZoom.orthographicSize = Mathf.SmoothDamp(cameraToZoom.orthographicSize, targetSize, ref currentVelocity, smoothTime);
+            yield return null;
+        }
+    }
+}
