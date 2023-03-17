574e44da38c4ede96937ab3b16b0c36e9c1749aa
Sitta Sahathum <sittasahathum@gmail.com>
2023-03-18 01:08:35 +0700

    add scene one

--- Assets/removeCam.cs
+++ Assets/removeCam.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class removeCam : MonoBehaviour
 {
-    public int timeToremove;
+    public float timeToremove;
     private void Start()
     {
         StartCoroutine(ExampleCoroutine());
