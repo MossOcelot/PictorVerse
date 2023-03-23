e420479f771d6293f4bfd1045f90601a247948fa
Sitta Sahathum <sittasahathum@gmail.com>
2023-03-24 04:24:36 +0700

    add page start

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public GameObject Progressshow;
+    
+        public void LoadGame()
+        {
+            StartCoroutine((IEnumerator)LoadGameAfterDelay());
+        }
+        private IEnumerator LoadGameAfterDelay()
+        {
+            yield return new WaitForSeconds(5f);
+            SceneManager.LoadScene(1);
+        }
+    
+    public void Active()
+    {
+        Progressshow.SetActive(true);
+    }
+
+}
